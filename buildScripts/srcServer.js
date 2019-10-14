39d546fa1273e64b74f86cacce6f48e4d406389b
Brandi Apetsi <brandiapetsi@brandis-mbp.raleigh.ibm.com>
2019-10-14 11:47:34 -0400

    added src file, index.html, and installed zeit now

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,16 @@
+//this file  will configure our webserver that will serve up the files in othe src directory
+
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+var port = 3000;
+var app = express();
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../src/index.html'))
+});
+
+app.listen(port, function(err) {
+  err ? console.log(err) : open('http://localhost:' + port)
+});

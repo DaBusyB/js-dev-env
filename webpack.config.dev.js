b68c524326940b81298a5fd6d097159c996b4247
Brandi Apetsi <brandiapetsi@brandis-mbp.raleigh.ibm.com>
2019-10-16 14:41:22 -0400

    configured webpack.config file

---
+++ webpack.config.dev.js
@@ -0,0 +1,35 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+  debug: true,
+  devtool: 'inline-source-map',
+  noInfo: false,
+
+  //applications entry point
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+
+  //defines where webpack should create the dev bundle
+  //there will not be a physical file, webpack just creates in and serves the bundles from memory
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src'),
+  },
+  plugins: [],
+
+  //tells webpack the file types(loaders) that we want it to handle
+  //webpack can handle many more types of loaders than this
+  module: {
+    loaders:[
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style', 'css']}
+    ]
+  }
+}

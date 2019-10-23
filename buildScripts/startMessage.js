04e6454708c75fdfcd002bd177853a63e4efb118
Brandi Apetsi <brandiapetsi@brandis-mbp.raleigh.ibm.com>
2019-10-23 13:37:28 -0400

    added eslint and esw, clean up some warnings

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,5 @@
 import chalk from 'chalk';
 
+/* eslint-disable no-console */
+
 console.log(chalk.magenta("Starting app in dev mode..."))
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 //per the webpack.config file, this is the entry point of the application
+import './index.css';
 
 import numeral from 'numeral';
 
+/* eslint-disable no-console */
+
 const courseValue = numeral(1000).format('$0,0.00');
+debugger;
 console.log(`I would pay ${courseValue} for this awesome course!`);
 

e5209285b2ec60decdce2c3e99b1182f92e460de
Brandi Apetsi <brandiapetsi@brandis-mbp.raleigh.ibm.com>
2019-10-24 10:44:17 -0400

    imported jsdom and ran basic test

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,23 @@
 import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+//fs is filesystem it allow us to interact with the filesystem using node and it comes from node
+
 
 describe('Our first test', () => {
   it('should pass', () => {
     expect(true).to.equal(true);
   });
 });
+
+describe('index.html', () => {
+  it('should say hello', (done) => {
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+    jsdom.env(index, function(err, window) {
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal("World");
+      done();
+      window.close();
+    });
+  })
+})
